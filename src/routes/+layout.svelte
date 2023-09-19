b420afdd38da0457f6a2592cc562893c829c1a84
AnonymousGCA <39743848+GABRYCA@users.noreply.github.com>
2023-09-19 23:33:46 +0200

    Changelogs: - Fixed navbar. - Minor changes
    TODO:
    - Update homepage with new dishes (based on menu).

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -33,17 +33,17 @@
 <!-- Navbar -->
 <nav class="navbar navbar-expand-lg navbar-light bg-light pb-2 pt-2 pb-lg-1 pt-lg-1 shadow">
     <div class="container">
-        <a class="navbar-brand" href="/">Ristorante Aquazzurra</a>
+        <a class="navbar-brand" href="/">{$t('locale.NavbarTitle')}</a>
         <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item">
-                    <a class="nav-link rounded-5 px-lg-4" href="/">{$t('locale.NavbarTitle')}</a>
+                    <a class="nav-link rounded-5 px-lg-4" href="/">{$t('locale.NavbarHome')}</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link rounded-5 px-lg-4" href="/galleria">{$t('locale.NavbarHome')}</a>
+                    <a class="nav-link rounded-5 px-lg-4" href="/galleria">{$t('locale.NavbarGallery')}</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link rounded-5 px-lg-4" href="/contatti">{$t('locale.NavbarContacts')}</a>
